d752680cf356c0493ae641029d3aef08d603b8c4
Tauber Krisztián <wk3zg9@inf.elte.hu>
2021-03-17 19:55:48 +0100

    GameState Class v2

--- backend/src/hu/g14de/GameState.java
+++ backend/src/hu/g14de/GameState.java
@@ -1,29 +1,111 @@
 package hu.g14de;
 
+import java.math.BigInteger;
 import java.util.Collection;
 
 public class GameState
 {
+    /**
+     * Unique ID
+     */
+    private String id;
+    /**
+     * Player given name
+     */
     private String name;
-    private long start;
-    private boolean started;
+    /**
+     * Money balance
+     */
+    private BigInteger balance;
+    /**
+     * The actual map
+     */
     private Map map;
-    private Scheduler scheduler;
-    private Balance balance;
+    /**
+     * Buildings
+     */
+    private BuildingCatalog buildingCatalog;
+    /**
+     * Guests who are actually visiting the park
+     */
     private Collection<Guest> guests;
-    private Collection<BuildingTemplate> buildingCatalog;
+    /**
+     * Responsible for ticking
+     */
+    private Scheduler scheduler;
+    /**
+     * Is the park opened
+     */
+    private boolean started;
+    /**
+     * Timestamp Creation Date
+     */
+    private long startTime;
 
-    public getStart();
-    public String getName();
-    public void setName();
-    public void notifyTick();
-    public long getStart();
-    public String getName();
-    public void setName();
-    public void notifyTick();
-    public void startPark();
-    public Balance getBalance();
-    public Scheduler getScheduler();
-    public Map getMap();
-    public Collection<BuildingTemplate> getBuildingCatalog();
+    public GameState()
+    {
+
+    }
+
+//Getters
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public BigInteger getBalance()
+    {
+        return balance;
+    }
+
+    public Map getMap()
+    {
+        return map;
+    }
+
+    public BuildingCatalog getBuildingCatalog()
+    {
+        return buildingCatalog;
+    }
+
+    public Scheduler getScheduler()
+    {
+        return scheduler;
+    }
+
+    public long getStartTime()
+    {
+        return startTime;
+    }
+
+//Setters
+    public void setName(String name)
+    {
+        //Checks...
+        this.name = name;
+    }
+
+//Methods
+
+    /**
+     * Catches Ticks
+     */
+    public void notifyTick()
+    {
+
+    }
+
+    /**
+     * Opens park
+     */
+    public void startPark()
+    {
+
+    }
+
 }
