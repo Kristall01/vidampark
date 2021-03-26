773abaac84cdadc3ecaf0b7dff3fa8669829fb8d
Tauber Krisztián <wk3zg9@inf.elte.hu>
2021-03-26 09:26:20 +0100

    BuildingInstance, Placeable, Road ArrayList update to List

--- backend/src/hu/g14de/Road.java
+++ backend/src/hu/g14de/Road.java
@@ -1,19 +1,29 @@
 package hu.g14de;
 
+import java.util.List;
+
 import static hu.g14de.Utils.checkNull;
 
-public class Road
+public class Road implements Placeable
 {
-    private Cell occuppiedCell;
+    private Cell occupiedCell;
+    private final int price;
 
     public Road(Cell cell)
     {
         checkNull(cell);
-        this.occuppiedCell = cell;
+        this.occupiedCell = cell;
+        price = 100;
     }
 
     public Cell getCell()
     {
-        return occuppiedCell;
+        return occupiedCell;
+    }
+
+    @Override
+    public List<Cell> cellsOccupied()
+    {
+        return List.of(occupiedCell);
     }
 }
