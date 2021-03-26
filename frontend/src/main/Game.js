51e74dbdbad1e04ba3653ab8fd1d12908ac4449e
Dudás Dominik <tk6t7c@inf.elte.hu>
2021-03-26 10:22:55 +0100

    frontend update

--- frontend/src/main/Game.js
+++ frontend/src/main/Game.js
@@ -1,7 +1,9 @@
 class Game {
 
 	constructor(game) {
-		
+		let unimplemented = () => alert("ez a funkctió még nem elérhető.");
+		document.getElementById("openpark-btn").addEventListener("click", unimplemented);
+		document.getElementById("catalog-btn").addEventListener("click", unimplemented);
 	}
 
 }
