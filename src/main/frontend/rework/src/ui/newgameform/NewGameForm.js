1da1ab7fe20f268f158c86b50d8de7b2e8c7485c
Tauber Krisztián <wk3zg9@inf.elte.hu>
2021-04-21 00:13:19 +0200

    Create game window

---
+++ src/main/frontend/rework/src/ui/newgameform/NewGameForm.js
@@ -0,0 +1,33 @@
+import { Component } from "react"
+import "./NewGameForm.css"
+import Window from "ui/window/Window"
+
+class NewGameForm extends Component {
+
+    render() {
+        let {hidden, closeWindow, okWindow} = this.props;
+        
+        return(
+            <div className="new-game-form">
+                <Window onclose={closeWindow} hidden={hidden} height="200px" width="465px" bgColor="#f3f3f3" borderColor="#9147ff" title="Új játék adatainak megadása">
+                    <div className="labels">
+                        <label>Név</label>
+                        <label>Magasság</label>
+                        <label>Szélesség</label>
+                    </div>
+                    <div className="inputs">
+                        <input type="text" value="Név" />
+                        <input type="number" value="10" />
+                        <input type="number" value="20" />
+                    </div>
+                    <div className="buttons">
+                        <button onClick={okWindow}>Létrehoz</button>
+                        <button onClick={closeWindow}>Mégse</button>
+                    </div>
+                </Window>
+            </div>
+        );
+    }
+}
+
+export default NewGameForm;
